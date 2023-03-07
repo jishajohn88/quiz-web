7c1be0406f7e538676cb73668a38d25a6e647277
Jisha Joshi <jisha.joshi@freedom-finance.com>
2023-03-07 13:43:34 +0000

    Changed the background color

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://quizapixyz-env-1.eba-ejkdbem2.eu-west-2.elasticbeanstalk.com/';
+    service.apiBase = 'https://quizapixyz-env-1.eba-ejkdbem2.eu-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
